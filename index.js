20e3e8d94903aa67e0dfb53a70d1243e81ebadd4
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2024-05-08 13:32:20 +0530

    latest

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-const http = require("http");
-const httpProxy = require("http-proxy");
-const url = require("url");
-
-// const sandbox = {
-//   THANAL_URL: "[2403:a080:c04:46b0:4c48:38f1:e8ab:dbe]",
-//   THANAL_NEXT_PORT: 3000,
-//   THANAL_API_PORT: 5000,
-// };
-// const proxy = httpProxy.createProxyServer({});
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  console.log(`Incoming request: ${req?.method} ${parsedUrl?.path}`);
-//   if (req.method === "GET" && parsedUrl.pathname === "/updateAddress") {
-    const ipv6 = parsedUrl?.query?.address || 123;
-    // sandbox.THANAL_URL = `[${ipv6}]`;
-    const responseData = { message: "Successfully updated url", ipv6 };
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(responseData));
-//   }
-//   proxy.web(req, res, {
-//     target: 'http://[2403:a080:c04:46b0:4c48:38f1:e8ab:dbe]:3000',
-//   });
+const express = require("express");
+const proxy = require("express-http-proxy");
+const sandbox = {};
+const app = express();
+const targetURL = "http://[2403:a080:c04:46b0:4c48:38f1:e8ab:dbe]:3000";
+const proxyMiddleware = proxy(targetURL);
+app.use("/", proxyMiddleware);
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Proxy server started on port ${port}`);
 });
-// proxy.on("error", (err, req, res) => {
-//   console.error("Proxy error:", err);
-//   res.writeHead(500, { "Content-Type": "text/plain" });
-//   res.end("Proxy Error");
-// });
-
-// Listen for server errors
-server.on("error", (err) => {
-  console.error("Server error:", err);
-});
-
-server.listen(8000);
-
-console.log("Tunnel server running on port 8000");
