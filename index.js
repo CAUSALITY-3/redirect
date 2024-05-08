badff31280bf51bad2aeff88217436c0c0df5cc0
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2024-05-08 13:34:45 +0530

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const proxy = require("express-http-proxy");
 const sandbox = {};
 const app = express();
-const targetURL = "http://[2403:a080:c04:46b0:4c48:38f1:e8ab:dbe]:3000";
+const targetURL = "www.google.com";
 const proxyMiddleware = proxy(targetURL);
 app.use("/", proxyMiddleware);
 const port = 8000;
