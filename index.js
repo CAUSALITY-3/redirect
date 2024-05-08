45920d65b6cd55ba756bd48b29fb45a7928da693
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2024-05-08 13:52:03 +0530

    ip logging

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const os = require('os');
+
+const interfaces = os.networkInterfaces();
+console.log("ip details", JSON.stringify(interfaces))
+// const wifi = interfaces?.["Wi-Fi"]?.find(items=> items.family === 'IPv6' && !items.internal && (items.cidr.includes("/64") || items.cidr.includes("/128")));
+// if (wifi) {
+//     console.log("ipv6 : ", wifi.address)
+// }
 const express = require("express");
 const proxy = require("express-http-proxy");
 const sandbox = {};
