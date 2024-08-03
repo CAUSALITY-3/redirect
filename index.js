40b97219eb9fed698c3c45738123d8f5ad7bbe6d
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2024-08-03 15:14:27 +0530

    simple proxy

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-const os = require('os');
+const http = require("http");
+const httpProxy = require("http-proxy");
+const url = require("url");
 
-const interfaces = os.networkInterfaces();
-console.log("ip details", JSON.stringify(interfaces))
-// const wifi = interfaces?.["Wi-Fi"]?.find(items=> items.family === 'IPv6' && !items.internal && (items.cidr.includes("/64") || items.cidr.includes("/128")));
-// if (wifi) {
-//     console.log("ipv6 : ", wifi.address)
-// }
-const express = require("express");
-const proxy = require("express-http-proxy");
-const sandbox = {};
-const app = express();
-const targetURL = "www.google.com";
-const proxyMiddleware = proxy(targetURL);
-app.use("/", proxyMiddleware);
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Proxy server started on port ${port}`);
+const sandbox = {
+  THANAL_URL: "localhost",
+  THANAL_NEXT_PORT: 3000,
+  THANAL_API_PORT: 5000,
+};
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer((req, res) => {
+  try {
+    const parsedUrl = url.parse(req.url, true);
+    console.log(`Incoming request: ${req.method} ${parsedUrl.path}`);
+    if (
+      req.method === "GET" &&
+      parsedUrl.pathname.startsWith("/images/getImage")
+    ) {
+      proxy.web(req, res, {
+        target: `http://${sandbox.THANAL_URL}:${sandbox.THANAL_API_PORT}`,
+      });
+    } else {
+      proxy.web(req, res, {
+        target: `http://${sandbox.THANAL_URL}:${sandbox.THANAL_NEXT_PORT}`,
+      });
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+  }
 });
+
+proxy.on("error", (err, req, res) => {
+  console.error("Proxy error:", err);
+  res.writeHead(500, { "Content-Type": "text/plain" });
+  res.end("Proxy Error");
+});
+
+server.on("error", (err) => {
+  console.error("Server error:", err);
+});
+
+server.listen(80);
+
+console.log("Tunnel server running on port 8000");
